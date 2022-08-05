80f77ac2dfe10f7ac5a80a7b2803f77f88ba87fa
Reid Simonton <simonton@gmail.com>
2022-08-05 14:13:44 -0700

    Ropsten

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+/* localhost
 module.exports = {
   solidity: "0.8.9",
   paths: {
@@ -11,4 +12,20 @@ module.exports = {
       chainId: 1337
     }
   }
+};
+*/
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/your-project-id",
+      accounts: [`0x${your-private-key}`]
+    }
+  },
+  solidity: "0.8.4",
 };
